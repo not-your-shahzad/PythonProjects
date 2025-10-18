a1aae7fe86d836e967c67f6bacb6343d49c54fc3
not-your-shahzad <shahzadhussain74303@gmail.com>
2025-10-18 07:21:52 +0500

    Lists in Python

---
+++ PythonProject/Lists_01.py
@@ -0,0 +1,20 @@
+# my_list = ["Wall", "Door", "Window", "Glass", "Mirror", "Kitchen"]
+# # my_list_1 = list()
+#
+# # print(my_list_1)
+# print(my_list)
+# print(my_list[0])
+# print(my_list[-1])
+# print(my_list[2:])#from the second index to last
+# print(my_list[:2]) #till second index
+# print(my_list[2:3]) #incluse second and exclude 3
+# print(my_list[::2])#freom zero to include second
+# print(my_list[2::])#freom second to last
+# print(my_list[::-1]) # reverse but also include first from reverse
+# print(my_list[2:4:2]) #print every second and include second until exclude four
+
+
+my_list = ["Hello", "World", "Python", "Snake", "Captain"]
+print(my_list)
+
+#Popoular functions for lists
